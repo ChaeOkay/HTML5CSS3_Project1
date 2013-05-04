6465072cdef0b5022de6bb26fd90dfffbff3ef35
cokeefe <chaemobile@gmail.com>
2013-05-04 16:04:18 -0400

    update css add jquery ext sheet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// JQuery test
 $(document).ready(function(){
 	alert(1);
 });
+
+/*
+NOT WORKING
+$(document).ready(function(){
+	$('#nav_li').hover(
+		function(){
+			console.log( "mouseneter");
+		$(this).addClass('.nav_li_hover');
+		},
+		function(){
+			console.log( "mouseleave");
+		$(this).removeClass('.nav_li_hover');	
+		},
+	);
+});
+*/
